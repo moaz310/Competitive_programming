801e31ef151c234e0dc45fe47304c8ac599d56b8
Moaz Mohammed <moazamar310@gmail.com>
2020-08-16 23:16:04 +0200

    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=503

--- DP/562-Dividingcoins.cpp
+++ DP/562-Dividingcoins.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int m;
+int bag[102];
+int memo[102][50005];
 
+int dp(int idx, int diff){
+    if(idx == m){
+        return diff;
+    }
+    int &res = memo[idx][diff];
+    if(res != 1e6){
+        return res;
+    }
+    return res = min(res, min(dp(idx+1, abs(bag[idx]-diff)), dp(idx+1, abs(bag[idx]+diff))));
+}
+
+
+int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        for(int i = 0; i < 102; i++){
+            for(int j = 0; j < 50005; j++){
+                memo[i][j] = 1e6;
+            }
+        }
+        scanf("%d", &m);
+        for(int i = 0; i < m; i++){
+            scanf("%d", &bag[i]);
+        }
+        printf("%d\n", dp(0, 0));
+    }
+    return 0;
+}
