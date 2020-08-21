7d078c76fc6802e8de07648758f69cdbb4467e20
Moaz Mohammed <moazamar310@gmail.com>
2020-08-21 10:17:24 +0200

    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3654

---
+++ DP/1213-SumofDifferentPrimes.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MAX = 1200;
+bool prime[MAX];
+vector<int> p;
+void seive(int n = MAX){
+    memset(prime, 1, sizeof prime);
+    int sq = sqrt(n);
+    prime[0] = prime[1] = 0;
+    for(int i = 0; i <= sq; i++){
+        if(prime[i]){
+            for(ll j = i*i; j < n; j+=i){
+                prime[j] = 0;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        if(prime[i]){
+            p.push_back(i);
+        }
+    }
+}
+
+int n, k;
+int memo[MAX][MAX][20];
+int dp(int idx = 0, int sum = 0, int len = 0){
+    ///cout << idx << "  " <<len << endl;
+    if(idx > p.size() || len > k || sum > n){
+        return 0;
+    }
+    if(sum == n && len == k){
+        return 1;
+    }
+    int &res = memo[idx][sum][len];
+    if(res != -1){
+        return res;
+    }
+    return res = dp(idx+1, sum + p[idx], len+1) + dp(idx+1, sum, len);
+}
+int main(){
+    seive();
+    while(scanf("%d %d", &n, &k) == 2 && n){
+        memset(memo, -1, sizeof memo);
+        printf("%d\n", dp());
+    }
+    return 0;
+}
